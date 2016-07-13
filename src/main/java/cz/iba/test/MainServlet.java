85cb6c232acd757e69f3fc598bbb25211f91454f
sylen1 <zeke4545@hotmail.com>
2016-07-13 08:36:21 +0200

    Level 4 - Využitie Spring MVC

--- src/main/java/cz/iba/test/MainServlet.java
+++ src/main/java/cz/iba/test/MainServlet.java
@@ -1,22 +1,23 @@
 package cz.iba.test;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Optional;
 import org.apache.commons.lang3.math.NumberUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
-public class MainServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int number = Optional.ofNullable(req.getParameter("x"))
-                .map(x -> NumberUtils.toInt(x, 1))
-                .filter(x -> x >= 0)
-                .orElse(1);
+@Controller
+public class MainServlet {
 
-        req.setAttribute("number", number);
-        getServletContext().getRequestDispatcher("/hello.jsp").forward(req, resp);
+    @RequestMapping("/")
+    public ModelAndView helloIba(@RequestParam(defaultValue = "1") String x){
+        ModelAndView modelAndView = new ModelAndView("hello.jsp");
+
+        int number = NumberUtils.toInt(x, 1);
+        number = number < 0 ? 1 : number;
+
+        modelAndView.addObject("number", number);
+
+        return modelAndView;
     }
 }
