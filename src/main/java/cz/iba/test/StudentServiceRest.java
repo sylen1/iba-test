6b6fa05c64b75545ba52419b3083db9da064e8c7
sylen1 <zeke4545@hotmail.com>
2016-07-14 20:27:46 +0200

    Level 10 - REST

---
+++ src/main/java/cz/iba/test/StudentServiceRest.java
@@ -0,0 +1,55 @@
+package cz.iba.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/")
+public class StudentServiceRest {
+
+    @Autowired
+    private StudentService studentService;
+
+    @RequestMapping("/all")
+    public List<Student> findAll() {
+        return studentService.findAll();
+    }
+
+    @RequestMapping("/findById")
+    public Student getById(@RequestParam int id) {
+        return studentService.getById(id);
+    }
+
+    @RequestMapping("/add")
+    public void add(@RequestBody Student student) {
+        studentService.add(student);
+    }
+
+    @RequestMapping("/addAll")
+    public void addAll(@RequestBody Collection<Student> students) {
+        studentService.addAll(students);
+    }
+
+    @RequestMapping("/removeByExample")
+    public void remove(@RequestBody Student student) {
+        studentService.remove(student);
+    }
+
+    @RequestMapping("/removeById")
+    public void remove(@RequestParam int id) {
+        studentService.remove(id);
+    }
+
+    @RequestMapping("/removeAll")
+    public void removeAll(@RequestBody Collection<Student> students) {
+        studentService.removeAll(students);
+    }
+
+    @RequestMapping("/update")
+    public void update(@RequestBody Student student) {
+        studentService.update(student);
+    }
+}
